f0348a91c0018bb57974207775f74996b5e83f9a
Usuario <geral.gonzalezs@duocuc.cl>
2026-04-27 23:17:38 -0400

    Cargar proyecto pagos

---
+++ src/main/java/com/cl/sda1085/pagos_proyecto/PagosProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.cl.sda1085.pagos_proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagosProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagosProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cl/sda1085/pagos_proyecto/PagosProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cl.sda1085.pagos_proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagosProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
